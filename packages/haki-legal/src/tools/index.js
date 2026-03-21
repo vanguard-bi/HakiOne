e8ee0e970f25ff7d24c6b0ef926af0e990739400
Roger <roger@vanguard.bi>
2026-03-21 05:58:04 +0300

    Remove residual local MCP code

--- packages/haki-legal/src/tools/index.js
+++
@@ -1,7 +0,0 @@
-import { SEARCH_TOOL_DEFINITION, handleSearch } from "./search.js";
-
-export const tools = [SEARCH_TOOL_DEFINITION];
-
-export const handlers = {
-  [SEARCH_TOOL_DEFINITION.name]: handleSearch,
-};
